64660fedbaf57f3a6d698585ead4051fb604f067
Thiago Pantoja <thiago.pantoja@easynext.com.br>
2025-03-21 03:13:35 -0300

    Initial Commit

---
+++ MIniCursoCSharp.Communication/Requests/RequestClientJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MIniCursoCSharp.Communication.Requests
+{
+    public class RequestClientJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
